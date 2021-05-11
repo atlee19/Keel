6cc4b0d0a2f6f31c87e7c7e8110bf64056395cbe
atlee19 <atleegraham16@gmail.com>
2021-05-11 19:33:46 -0400

    got electron.js boilerplate running

---
+++ main.js
@@ -0,0 +1,38 @@
+/**
+ * Author:    Graham Atlee
+ * Created:   05.11.2021
+ * 
+ * GNU Affero General Public License v3.0
+ * 
+ * This class specifies the entry point of the Electron application
+ **/
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
