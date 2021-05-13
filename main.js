01a789f439783251119d661ccfbaa25dba04f02f
atlee19 <atleegraham16@gmail.com>
2021-05-13 15:27:16 -0400

    added & sectioned auto updater code

--- main.js
+++ main.js
@@ -9,10 +9,17 @@
  * This class specifies the entry point of the Electron application
  **/
 const { app, BrowserWindow } = require('electron')
+const { autoUpdater } = require("electron-updater");
 const path = require('path')
 
+var win; //global window object
+
+//-------------------------------------------------------------------
+// Code related to main window creation and closing 
+//-------------------------------------------------------------------
+
 function createWindow () {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -37,4 +44,44 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
+})
+
+
+//-------------------------------------------------------------------
+// Auto updates 
+//
+// This will immediately download an update, then quit and install
+//-------------------------------------------------------------------
+
+//send any info on updates to main window 
+function sendStatusToWindow(text) {
+    win.webContents.send('message', text);
+}
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdates();
+});
+
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    sendStatusToWindow('Downloading new update...');
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+  autoUpdater.quitAndInstall();  
 })
