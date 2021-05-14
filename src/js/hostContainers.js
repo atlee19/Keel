7d20c7927d23b3fb18212a7eb58439db71f5864b
atlee19 <atleegraham16@gmail.com>
2021-05-14 13:26:14 -0400

    function written for getting list of currently running containers

--- src/js/hostContainers.js
+++ src/js/hostContainers.js
@@ -12,6 +12,27 @@
  **/
 const Docker = require('dockerode');
 
+const socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+const docker = new Docker({ socketPath: socket });
+
 const hostContainers = {
-    //pass
+    listRunning = function(){
+        let runningContainers = [];
+       
+        docker.listContainers({ all : false }, function(err, containers){
+            //if no containers are running what gets displayed?
+            for(let container in containers){
+                //get name and date created 
+                let name = containers[container].Names[0];
+                let timeStamp = containers[container].Created;
+                //put it into a json object
+                let containerMetaData = { "name" : name, "created" : timeStamp };
+                //add it to array 
+                runningContainers.push(containerMetaData);
+                //then return it - thats it
+            }
+        })
+
+        return runningContainers;
+    }
 };
