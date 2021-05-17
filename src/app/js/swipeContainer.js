94c60c44e2467826611fd784bd2af0dfabba6361
atlee19 <atleegraham16@gmail.com>
2021-05-16 20:39:53 -0400

    added container swipe js

---
+++ src/app/js/swipeContainer.js
@@ -0,0 +1,13 @@
+/**
+ * Keel 2021 
+ * 
+ * Author:    Graham Atlee
+ * Created:   05.16.2021
+ * 
+ * GNU Affero General Public License v3.0
+ * 
+ * This class handles the mouse swiping interactions 
+ * involved with the container list displayed in the main window.
+ * There is to be NO logic involved with closing, starting etc. of 
+ * Docker containers
+ **/
