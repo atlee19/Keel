23b64a37f856d174e6c757cb4011b003a185d676
atlee19 <atleegraham16@gmail.com>
2021-05-22 18:38:57 -0400

    added display containers class

---
+++ src/app/js/displayContainers.js
@@ -0,0 +1,16 @@
+/**
+ * Keel 2021 
+ * 
+ * Author:    Graham Atlee
+ * Created:   05.22.2021
+ * 
+ * GNU Affero General Public License v3.0
+ * 
+ * This class handles anything that involes controling/displaying
+ * containers to the main window. This class directly interacts with
+ * the DOM.
+ **/
+
+const displayContainers = (function(){
+    //pass
+})();
