a00557b7f96a13f64c5b7309d7cb7379cea28e1d
atlee19 <atleegraham16@gmail.com>
2021-05-23 13:49:43 -0400

    create test containers

---
+++ create-test-containers.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SLEEP_TIME=300
+NUM_CONTAINERS=5
+i=0
+
+while [ $i -lt $NUM_CONTAINERS ]
+do
+    docker run -d flask-web:python sleep $SLEEP_TIME
+    ((i++))
+done
+
+
+echo 5 containers created.
