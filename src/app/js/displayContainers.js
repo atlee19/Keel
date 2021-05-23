74b0cfc6186d06550ea88e96bbe4e513e3502632
atlee19 <atleegraham16@gmail.com>
2021-05-23 17:28:14 -0400

    txt spacing

--- src/app/js/displayContainers.js
+++ src/app/js/displayContainers.js
@@ -24,10 +24,10 @@ const displayContainers = (function(){
                 <li id=${container.Id} class="activeContainer">${container.Name}
                     <ul class="containerMetaData">
                         <li style="display: inline;">
-                            <span style="color: #646464">image:</span>${container.Image}
+                            <span style="color: #646464">image:   </span>${container.Image}
                         </li>
                         <li style="display: inline; margin-left: 3%">
-                            <span style="color: #646464">id:</span>${container.Id}
+                            <span style="color: #646464">id:   </span>${container.Id}
                         </li>
                     </ul>
                 </li>`;
